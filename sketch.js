7b795c8e6692ff64a2aab3104236378029da3234
TreeAspen <sy4254@nyu.edu>
2024-09-24 21:42:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,43 +1,88 @@
-let angles = []; 
-let length = 100;
+// let angles = []; 
+// let length = 100;
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+//   angleMode(DEGREES); 
+
+//   for (let a = 0; a < windowWidth; a += length) {
+//     let row = [];
+//     for (let b = 0; b < windowHeight; b += length) {
+//       let c = random([0, 1, 2, 3]);
+//       row.push(90 * c);
+//     }
+//     angles.push(row);
+//   }
+// }
+
+// function draw() {
+//   background(71, 158, 151);
+
+//   function grid(c1, c2, x, y, a, distance, angle) {
+//     push();
+//     fill(c1);
+//     noStroke();
+//     rect(x, y, a, a);
+//     fill(c2);
+//     translate(x + a / 2, y + a / 2);
+//     rotate(angle);
+//     quad(-a / 2, -a / 2, a / 2, -a / 2, a / 2 - distance, a / 2 - distance, -a / 2, a / 2);
+//     fill(c1);
+//     ellipse(-a / 2 + distance + (mouseX - windowWidth / 2) /windowWidth*distance, -a / 2 + distance + (mouseY - windowHeight / 2) /windowWidth*distance, distance/1.5);
+//     pop();
+//   }
+
+//   for (let a = 0; a < windowWidth; a += length) {
+//     for (let b = 0; b < windowHeight; b += length) {
+//       let i = a / length;
+//       let j = b / length;
+//       let angle = angles[i][j]; 
+//       grid('black', 'white', a, b, length, 30, angle);
+//     }
+//   }
+// }
+
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES); 
-
-  for (let a = 0; a < windowWidth; a += length) {
-    let row = [];
-    for (let b = 0; b < windowHeight; b += length) {
-      let c = random([0, 1, 2, 3]);
-      row.push(90 * c);
-    }
-    angles.push(row);
   }
-}
+
 
 function draw() {
   background(71, 158, 151);
-
-  function grid(c1, c2, x, y, a, distance, angle) {
+  function flower(color1,color2,x,y,length,angle){
     push();
-    fill(c1);
     noStroke();
-    rect(x, y, a, a);
-    fill(c2);
-    translate(x + a / 2, y + a / 2);
+    fill(color1);
+
+    translate(x, y);
     rotate(angle);
-    quad(-a / 2, -a / 2, a / 2, -a / 2, a / 2 - distance, a / 2 - distance, -a / 2, a / 2);
-    fill(c1);
-    ellipse(-a / 2 + distance + (mouseX - windowWidth / 2) /windowWidth*distance, -a / 2 + distance + (mouseY - windowHeight / 2) /windowWidth*distance, distance);
-    pop();
+    for(a = 0;a < 270 ;a +=90){
+      push();
+      rotate(a);
+      beginShape();
+      vertex(0, 4*length);
+      vertex(-length, 4*length);
+      vertex(-3*length, 4*length);
+      vertex(-3*length, 3*length);
+      vertex(-4*length, 3*length);
+      vertex(-4*length, length);
+      vertex(0, 4*length);
+      endShape();
+      fill(color2)
+      triangle(0,0,-5*length,0,-3*length,length)
+      triangle(0,0,0,5*length,-length,3*length)
+
+      pop()
+
+    }
+    pop()
+  flower('black','white',windowWidth/2,windowHeight/2,10,0)  
+
   }
 
-  for (let a = 0; a < windowWidth; a += length) {
-    for (let b = 0; b < windowHeight; b += length) {
-      let i = a / length;
-      let j = b / length;
-      let angle = angles[i][j]; 
-      grid('black', 'white', a, b, length, 30, angle);
-    }
-  }
+
+
+
 }
