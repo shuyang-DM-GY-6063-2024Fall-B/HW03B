136be748f0df9f120e24587183dc1c193efeea9a
TreeAspen <sy4254@nyu.edu>
2024-09-26 13:29:33 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,6 +57,8 @@ function draw() {
   function flower(color1,color2,x,y,length,angle){
     push();
     noStroke();
+    // stroke(color2);
+    // strokeWeight(2);
     fill(color1);
 
     translate(x, y);
@@ -106,11 +108,11 @@ function draw() {
       let e = b/length/8%2+1
       // let direction = random([1, -1])
       if((e+d)%2 === 1){
-        flower(color2, color4, a, b, 6, -x*d*e*2);
+        flower(color2, color4, a, b, 4, -x*d*e*5);
          }
       else{
-        flower( color3, color5, a, b, 12, x*d*e+45);
-        flower( color1, color6, a, b, 6, x*d*e);
+        flower( color3, color5, a, b, 12, x*d*e*2+45);
+        flower( color1, color6, a, b, 6, -x*d*e);
       }
       }
       
